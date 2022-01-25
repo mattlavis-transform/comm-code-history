135ac030758a0cebbdc285551301c18e68aa3239
mattlavis-transform <matthew.lavis@gmail.com>
2022-01-25 23:19:05 +0000

    Quotas

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 # app.py
-from flask import Flask, request, jsonify
-import os
-from datetime import datetime
-from collections import OrderedDict
-from classes.commodity import Commodity
-from classes.commodity2 import Commodity2
-from classes.sqlite_helper import DatabaseLite
+from flask import Flask, request
 
 
 import globals as g
@@ -13,135 +7,35 @@ import globals as g
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-def to_yyyymmdd(s):
-    if s is None:
-        s = ""
-    else:
-        if isinstance(s, str):
-            s = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
-        
-        s = datetime.strftime(s, "%Y-%m-%d")
-    return s
-    
-def to_display(s):
-    if s is None:
-        s = ""
-    else:
-        if isinstance(s, str):
-            s = datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
 
-        s = datetime.strftime(s, "%d %B %Y")
-    return s
-
-
-# A welcome message to test our server
 @app.route('/')
 def index():
-    a = "Hello"
-    b = " you"
-    return g.do_append(a, b)
-    # return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Welcome to our server !!</h1>"
 
-@app.route('/xcommodities', methods=['GET'])
-def get_commodity():
-    commodity_code = request.args.get('c')
-    c = Commodity(commodity_code)
-    return c.data
-
-@app.route('/sqlite3')
-def sqlite3():
-    database_filename = os.path.join(os.getcwd(), "db", "commodity-code-history.db")
-    db = DatabaseLite(database_filename)
-    sql = "select * from goods_nomenclatures limit 10;"
-    rows = db.run_query(sql)
-
-    return "<h1>SQLite</h1>"
 
 @app.route('/commodities')
-def commodities_local():
-    data = {}
-    instances = []
-    commodity_code = request.args.get('c')
-
-    database_filename = os.path.join(os.getcwd(), "db", "commodity-code-history.db")
-    db = DatabaseLite(database_filename)
-
-    sql = """select goods_nomenclature_sid, goods_nomenclature_item_id, validity_start_date, validity_end_date 
-    from goods_nomenclatures
-    where goods_nomenclature_item_id = '""" + commodity_code + """'
-    order by validity_start_date desc;
-    """
-    rows = db.run_query(sql)
-    for row in rows:
-        instance = {
-            "sid": row[0],
-            "goods_nomenclature_item_id": row[1],
-            "validity_start_date": to_yyyymmdd(row[2]),
-            "validity_end_date": to_yyyymmdd(row[3]),
-            "validity_start_date_display": to_display(row[2]),
-            "validity_end_date_display": to_display(row[3])
-        }
-        instances.append(instance)
-    data["data"] = instances
-
-    return data
+def get_commodity_history():
+    commodity_history = g.app.get_commodity_history()
+    return commodity_history
 
 
-@app.route('/measure_types')
-def measure_types():
+@app.route('/measure-types')
+def get_measure_types():
     measure_types = g.app.get_measure_types()
     return measure_types
 
 
 @app.route('/document-codes')
-def document_codes_local():
-    # data = {}
-    data = OrderedDict()
-    instances = []
-    code = request.args.get('c')
+def get_document_codes():
+    document_codes = g.app.get_document_codes()
+    return document_codes
 
-    database_filename = os.path.join(os.getcwd(), "db", "commodity-code-history.db")
-    db = DatabaseLite(database_filename)
 
-    if code is None:
-        sql = """
-        select code, description, certificate_type_code, certificate_code, validity_start_date
-        from certificates c order by code;
-        """
-    else:
-        sql = """
-        select code, description, certificate_type_code, certificate_code, validity_start_date
-        from certificates c where code = '""" + code + """' order by code 
-        """
-        
-    rows = db.run_query(sql)
-    for row in rows:
-        instance = OrderedDict()
-        attributes = OrderedDict()
-        
-        attributes["certificate_type_code"] = row[2]
-        attributes["certificate_code"] = row[3]
-        attributes["description"] = row[1]
-        attributes["validity_start_date"] = row[4]
-        
-        instance["id"] = row[0]
-        instance["type"] = "certificates"
-        instance["attributes"] = attributes
-        
-        # instance = {
-        #     "id": row[0],
-        #     "type": "certificates",
-        #     "attributes": {
-        #         "certificate_type_code": row[2],
-        #         "certificate_code": row[3],
-        #         "description": row[1],
-        #         "validity_start_date": to_display(row[4])
-        #     }
-        # }
-        instances.append(instance)
-    data["data"] = instances
+@app.route('/quotas')
+def get_quotas():
+    quotas = g.app.get_quotas()
+    return quotas
 
-    return data
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
--- application.py
+++ application.py
@@ -1,21 +1,155 @@
 import os
 from datetime import datetime
 from flask import Flask, request, jsonify
+from markupsafe import re
 from classes.sqlite_helper import DatabaseLite
 
 
 class Application(object):
     def __init__(self):
-        pass
+        self.database_filename = os.path.join(os.getcwd(), "db", "commodity-code-history.db")
+
+    def get_commodity_history(self):
+        data = {}
+        instances = []
+        commodity_code = request.args.get('c')
+
+        db = DatabaseLite(self.database_filename)
+
+        sql = """select goods_nomenclature_sid, goods_nomenclature_item_id, validity_start_date, validity_end_date 
+        from goods_nomenclatures
+        where goods_nomenclature_item_id = '""" + commodity_code + """'
+        order by validity_start_date desc;
+        """
+        rows = db.run_query(sql)
+        for row in rows:
+            instance = {
+                "sid": row[0],
+                "goods_nomenclature_item_id": row[1],
+                "validity_start_date": self.to_yyyymmdd(row[2]),
+                "validity_end_date": self.to_yyyymmdd(row[3]),
+                "validity_start_date_display": self.to_display(row[2]),
+                "validity_end_date_display": self.to_display(row[3])
+            }
+            instances.append(instance)
+        data["data"] = instances
+
+        return data
+
+    def get_document_codes(self):
+        data = {}
+        instances = []
+        code = request.args.get('c')
+
+        db = DatabaseLite(self.database_filename)
+
+        if code is None:
+            sql = """
+            select code, description, certificate_type_code, certificate_code, validity_start_date
+            from certificates c order by code;
+            """
+        else:
+            sql = """
+            select code, description, certificate_type_code, certificate_code, validity_start_date
+            from certificates c where code = '""" + code + """' order by code 
+            """
+            
+        rows = db.run_query(sql)
+        for row in rows:
+            instance = {}
+            attributes = {}
+            
+            attributes["certificate_type_code"] = row[2]
+            attributes["certificate_code"] = row[3]
+            attributes["description"] = row[1]
+            attributes["validity_start_date"] = row[4]
+            
+            instance["id"] = row[0]
+            instance["type"] = "document_code"
+            instance["attributes"] = attributes
+            
+            instances.append(instance)
+            data["data"] = instances
+
+        return data
+
+    def get_quotas(self):
+        from_date = request.args.get('from')
+        to_date = request.args.get('to')
+
+        instances = []
+        data = {}
+        db = DatabaseLite(self.database_filename)
+        if from_date and to_date:
+            sql = """
+            select * from quota_order_numbers
+            where definition_start_date <= '""" + to_date + """'
+            and definition_end_date >= '""" + from_date + """'
+            order by quota_order_number_id, quota_start_date, definition_start_date
+            """
+        else:
+            sql = """
+            select * from quota_order_numbers
+            order by quota_order_number_id, quota_start_date, definition_start_date
+            """
+        
+        rows = db.run_query(sql)
+        previous_order_number = -1
+        for row in rows:
+            if previous_order_number != row[1]:
+                if previous_order_number != -1:
+                    instances.append(instance)
+
+                instance = {}
+                attributes = {}
+                relationships = []
+                
+                attributes["sid"] = row[0]
+                attributes["quota_start_date"] = self.to_yyyymmdd(row[2])
+                attributes["quota_end_date"] = self.to_yyyymmdd(row[3])
+                
+                relationship = self.get_relationship(row)
+                relationships.append(relationship)
+                
+                instance["id"] = row[1]
+                instance["type"] = "quota_order_number"
+                instance["attributes"] = attributes
+                instance["relationships"] = relationships
+            else:
+                relationship = self.get_relationship(row)
+                relationships.append(relationship)
+            
+            previous_order_number = row[1]
+
+        if instance:
+            instances.append(instance)
+            
+        data["data"] = instances
+
+        return data
     
+    def get_relationship(self, row):
+        relationship = {}
+        attributes = {}
+        relationship["id"] = row[4]
+        relationship["type"] = "quota_definition"
+        attributes["id"] = row[4]
+        attributes["validity_start_date"] = self.to_yyyymmdd(row[5])
+        attributes["validity_end_date"] = self.to_yyyymmdd(row[6])
+        attributes["initial_volume"] = row[7]
+        attributes["measurement_unit_code"] = row[8]
+        attributes["measurement_unit_qualifier_code"] = row[9]
+        attributes["maximum_precision"] = row[10]
+        attributes["critical_state"] = row[11]
+        attributes["critical_threshold"] = row[12]
+        relationship["attributes"] = attributes
+        return relationship
+
     def get_measure_types(self):
         data = {}
         instances = []
         snapshot_date = request.args.get('as_of')
-
-        database_filename = os.path.join(os.getcwd(), "db", "commodity-code-history.db")
-        db = DatabaseLite(database_filename)
-
+        db = DatabaseLite(self.database_filename)
         if snapshot_date:
             sql = """
             select measure_type_id, measure_type_description, measure_type_series_id,
@@ -36,17 +170,22 @@ class Application(object):
             
         rows = db.run_query(sql)
         for row in rows:
-            instance = {
-                "measure_type_id": row[0],
-                "measure_type_description": row[1],
-                "measure_type_series_id": row[2],
-                "measure_type_series_description": row[3],
-                "trade_movement_code": row[4],
-                "measure_component_applicable_code": row[5],
-                "order_number_capture_code": row[6],
-                "validity_start_date": self.to_yyyymmdd(row[7]),
-                "validity_end_date": self.to_yyyymmdd(row[8])
-            }
+            instance = {}
+            attributes = {}
+            
+            attributes["description"] = row[1]
+            attributes["measure_type_series_id"] = row[2]
+            attributes["measure_type_series_description"] = row[3]
+            attributes["trade_movement_code"] = row[4]
+            attributes["measure_component_applicable_code"] = row[5]
+            attributes["order_number_capture_code"] = row[6]
+            attributes["validity_start_date"] = self.to_yyyymmdd(row[7])
+            attributes["validity_end_date"] = self.to_yyyymmdd(row[8])
+            
+            instance["id"] = row[0]
+            instance["type"] = "measure_type"
+            instance["attributes"] = attributes
+            
             instances.append(instance)
         
         data["data"] = instances
